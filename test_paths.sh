1f2ca07c3ce123099c33a3c93e7dbf9096d52326
pradyot.o.c@gmail.com <48565460+PradyotC@users.noreply.github.com>
2026-04-19 04:34:34 +0530

    added test_paths.sh

---
+++ test_paths.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -v http://k8s-default-capstone-81f42579c4-1898413698.us-east-1.elb.amazonaws.com/
+curl -v http://k8s-default-capstone-81f42579c4-1898413698.us-east-1.elb.amazonaws.com/dashboard
+curl -v http://k8s-default-capstone-81f42579c4-1898413698.us-east-1.elb.amazonaws.com/api/v1/health
