efc1dce07fbfce9eddf875271a01a33ea260e48b
pradyot.o.c@gmail.com <48565460+PradyotC@users.noreply.github.com>
2026-04-19 12:02:46 +0530

    tested deployments

--- important_commands.sh
+++ important_commands.sh
@@ -4,7 +4,11 @@ docker run --rm -it \
   -v $(pwd)/k8s-manifests:/workspace/k8s-manifests \
   pradyotc/eks-deployer:latest
 
+kubectl get ingress capstone-ingress
+
 #test the links
-curl -v http://k8s-default-capstone-81f42579c4-1898413698.us-east-1.elb.amazonaws.com/
-curl -v http://k8s-default-capstone-81f42579c4-1898413698.us-east-1.elb.amazonaws.com/dashboard
-curl -v http://k8s-default-capstone-81f42579c4-1898413698.us-east-1.elb.amazonaws.com/api/v1/health
+curl -v http://k8s-default-capstone-81f42579c4-1854062926.us-east-1.elb.amazonaws.com/
+curl -v http://k8s-default-capstone-81f42579c4-1854062926.us-east-1.elb.amazonaws.com/dashboard
+curl -v http://k8s-default-capstone-81f42579c4-1854062926.us-east-1.elb.amazonaws.com/api/v1/health
+
+bash /usr/local/bin/destroy.sh
